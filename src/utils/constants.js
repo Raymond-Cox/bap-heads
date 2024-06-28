ea72c63035239165ac71c3c3d1850a0fe95285cf
Raymond Cox <raymondcdev@gmail.com>
2024-06-28 09:36:58 -0400

    Added July events

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -16,6 +16,19 @@ export const socials = [
 
 export const newsItems = [
   {
+    url: 'https://i.imgur.com/3zW6HMK.jpeg',
+    title: 'Bap Times Issue #2',
+    description:
+      'Read about the latest clan news, events, and more in this promising sequel of the Bap Times.',
+    date: '6/28/2024',
+  },
+  {
+    url: 'https://i.imgur.com/a96wqNR.png',
+    title: 'July Events',
+    description: 'Check out the events we have planned for the month of July.',
+    date: '6/28/2024',
+  },
+  {
     url: 'https://i.imgur.com/aYj11UR.png',
     title: 'Bap Times Issue #1',
     description:
