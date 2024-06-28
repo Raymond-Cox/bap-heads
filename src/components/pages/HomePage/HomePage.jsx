e93d0752e9e773bbdd9df70cfddeebc1ff04826d
Raymond Cox <raymondcdev@gmail.com>
2024-06-28 09:05:13 -0400

    Swap socials to logos

--- src/components/pages/HomePage/HomePage.jsx
+++ src/components/pages/HomePage/HomePage.jsx
@@ -1,44 +1,19 @@
 import { Fragment } from 'react'
-import { FiExternalLink } from 'react-icons/fi'
 import { NewsItem, ScrollBG } from '../../molecules'
+import { newsItems, socials } from '../../../utils'
 import * as classes from './HomePage.module.css'
 
-const socials = [
-  {
-    link: 'https://discord.gg/bapheads',
-    text: 'Discord',
-  },
-  {
-    link: 'https://wiseoldman.net/groups/5474',
-    text: 'WiseOldMan',
-  },
-]
-
-const items = [
-  {
-    url: 'https://i.imgur.com/aYj11UR.png',
-    title: 'Bap Times Issue #1',
-    description:
-      'Read about the latest clan news, events, and more in the first issue of the Bap Times.',
-    date: '6/1/2024',
-  },
-  {
-    url: 'https://i.imgur.com/R6DBVQ0.png',
-    title: 'June Events',
-    description: 'Check out the events we have planned for the month of June.',
-    date: '6/1/2024',
-  },
-]
-
+/**
+ * The home page of the BapHeads website
+ */
 const HomePage = () => {
   return (
     <ScrollBG>
       <h1>Welcome to BapHeads OSRS clan</h1>
       <div className={classes.socialsContainer}>
-        {socials.map(({ link, text }) => (
+        {socials.map(({ link, text, img }) => (
           <a key={link} href={link} target="_blank" rel="noopener noreferrer">
-            {text}
-            <FiExternalLink />
+            <img src={img} alt={text} />
           </a>
         ))}
       </div>
@@ -46,7 +21,7 @@ const HomePage = () => {
       <h3>News and Updates</h3>
 
       <div>
-        {items.map((item) => (
+        {newsItems.map((item) => (
           <Fragment key={item.url}>
             <NewsItem {...item} />
             <hr />
---
+++ src/utils/constants.js
@@ -0,0 +1,31 @@
+import discordIcon from '../assets/discord-mark-blue.svg'
+import womIcon from '../assets/wom_logo.png'
+
+export const socials = [
+  {
+    link: 'https://discord.gg/bapheads',
+    text: 'Discord',
+    img: discordIcon,
+  },
+  {
+    link: 'https://wiseoldman.net/groups/5474',
+    text: 'WiseOldMan',
+    img: womIcon,
+  },
+]
+
+export const newsItems = [
+  {
+    url: 'https://i.imgur.com/aYj11UR.png',
+    title: 'Bap Times Issue #1',
+    description:
+      'Read about the latest clan news, events, and more in the first issue of the Bap Times.',
+    date: '6/1/2024',
+  },
+  {
+    url: 'https://i.imgur.com/R6DBVQ0.png',
+    title: 'June Events',
+    description: 'Check out the events we have planned for the month of June.',
+    date: '6/1/2024',
+  },
+]
