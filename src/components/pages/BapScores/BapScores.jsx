f815029253857b4a05e4ffc75308deb0b9c7ef6e
Raymond Cox <raymondcdev@gmail.com>
2024-07-24 12:04:06 -0400

    Before branching off for article

--- src/components/pages/BapScores/BapScores.jsx
+++ src/components/pages/BapScores/BapScores.jsx
@@ -1,6 +1,13 @@
 import { useQuery } from '@tanstack/react-query'
+import {
+  VictoryBar,
+  VictoryChart,
+  VictoryStack,
+  VictoryZoomContainer,
+} from 'victory'
 import { ScrollBG } from '../../molecules'
 import { BapHeadAPI } from '../../../api'
+import * as classes from './BapScores.module.css'
 
 const BapScores = () => {
   const { data, isLoading } = useQuery({
@@ -10,10 +17,47 @@ const BapScores = () => {
 
   console.log(data)
 
+  const minigamesData = data?.map((item) => ({
+    x: item.username,
+    y: item.minigamesClues,
+  }))
+
+  const skillingData = data?.map((item) => ({
+    x: item.username,
+    y: item.skilling,
+  }))
+
+  const pvmData = data?.map((item) => ({
+    x: item.username,
+    y: item.pvm,
+  }))
+
   return (
     <ScrollBG>
-      <h3>Bap Scores</h3>
+      <div className={classes.header}>
+        <h3>Bap Scores</h3>
+      </div>
+
       {isLoading && <p>Loading - please wait...</p>}
+      {data && (
+        <VictoryChart
+          domainPadding={20}
+          containerComponent={
+            <VictoryZoomContainer
+              allowZoom={false}
+              zoomDomain={{ x: [0, 5] }}
+              zoomDimension="x"
+              responsive={false}
+            />
+          }
+        >
+          <VictoryStack colorScale="green">
+            <VictoryBar data={minigamesData} />
+            <VictoryBar data={skillingData} />
+            <VictoryBar data={pvmData} />
+          </VictoryStack>
+        </VictoryChart>
+      )}
     </ScrollBG>
   )
 }
