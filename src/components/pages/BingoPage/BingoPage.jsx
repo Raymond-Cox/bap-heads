a7ce2d9d80b9c40c4c29093794f2dfd41d83060c
Raymond Cox <raymondcdev@gmail.com>
2024-08-19 04:11:18 -0400

    Added Canva stats

--- src/components/pages/BingoPage/BingoPage.jsx
+++ src/components/pages/BingoPage/BingoPage.jsx
@@ -1,80 +1,54 @@
-import { useEffect, useState } from 'react'
 import { ScrollBG } from '../../molecules'
 import * as classes from './BingoPage.module.css'
-
-const startsAt = new Date('2024-08-05T15:00:00.000Z')
-const endsAt = new Date('2024-08-15T15:00:00.000Z')
-
-/**
- * Calculates the time left until the event starts
- * @param {Date} date
- */
-const calcTimeLeft = (date, stateString = 'started') => {
-  const now = new Date().getTime()
-  const distance = date.getTime() - now
-
-  const days = Math.floor(distance / (1000 * 60 * 60 * 24))
-  const hours = Math.floor(
-    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-  )
-  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
-  const seconds = Math.floor((distance % (1000 * 60)) / 1000)
-
-  if (days < 0 || hours < 0 || minutes < 0 || seconds < 0)
-    return `Event has ${stateString}`
-
-  return `${days}d ${hours}h ${minutes}m ${seconds}s`
-}
 
 /**
  * Displays current bingo information
  */
 const BingoPage = () => {
-  const [times, setTimes] = useState({
-    startTime: calcTimeLeft(startsAt),
-    endTime: calcTimeLeft(endsAt, 'ending'),
-  })
-
-  /**
-   * Updates the time left every second
-   */
-  useEffect(() => {
-    setInterval(() => {
-      setTimes({
-        startTime: calcTimeLeft(startsAt),
-        endTime: calcTimeLeft(endsAt, 'ending'),
-      })
-    }, 1000)
-  }, [])
-
-  const renderTimes = () => {
-    if (times.startTime !== 'Event has started')
-      return <h4>Starts: {times.startTime}</h4>
-
-    return <h4>Ends: {times.endTime}</h4>
-  }
-
   return (
     <ScrollBG>
       <div className={classes.container}>
-        <div className={classes.header}>
+        <div className={classes.innerContainer}>
           <h3>2024 Summer Bingo</h3>
-          {renderTimes()}
+          <p>
+            Summer bingo has ended. Congratulations to the winning teams, as
+            well as to each competitor for making the event so much fun.
+          </p>
+
+          <a
+            href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGOL61aiq4&#x2F;M6KIiTJ4hytbHeOgV0NRpw&#x2F;view?utm_content=DAGOL61aiq4&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={classes.link}
+          >
+            Bingo Stats
+          </a>
+          <div className={classes.canvas}>
+            <iframe
+              loading="lazy"
+              className={classes.iframe}
+              src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGOL61aiq4&#x2F;M6KIiTJ4hytbHeOgV0NRpw&#x2F;view?embed"
+              allowFullScreen
+              allow="fullscreen"
+            />
+          </div>
+
+          <hr />
 
           <a
             href="https://docs.google.com/spreadsheets/d/1i-uU3AFwac_241e-ybhGcItHkIO-ihOUaKlF-XP9mFY/edit?usp=sharing"
             target="_blank"
-            rel="noreferrer"
+            rel="noreferrer noopener"
+            className={classes.link}
           >
-            View full information
+            Full team results and logs
           </a>
+          <iframe
+            src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQKMKUVHFHn6PoBmE4ie9uKqKrbGFmr9cMM1rBzFvS-nO88nowXE8B8j3H08l6GVq25OBPY8K2Gp_CF/pubhtml?gid=822621149&amp;single=true&amp;widget=true&amp;headers=false"
+            width="100%"
+            height="700"
+          />
         </div>
-
-        <iframe
-          src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQKMKUVHFHn6PoBmE4ie9uKqKrbGFmr9cMM1rBzFvS-nO88nowXE8B8j3H08l6GVq25OBPY8K2Gp_CF/pubhtml?gid=822621149&amp;single=true&amp;widget=true&amp;headers=false"
-          width="100%"
-          height="700"
-        />
       </div>
     </ScrollBG>
   )
